a7132d573f6f54fd537fad096aadeebfe786af3e
Patrick <ox6185ys@go.minneapolis.edu>
2016-03-26 14:06:37 -0500

    Crazy Eights Project 1

---
+++ CrazyEights.java
@@ -0,0 +1,36 @@
+package com.patrick;
+
+public class CrazyEights {
+
+    public static void main(String[] args) {
+//Printing colors to the terminal
+        String ANSI_red = "\u001B[31m";        //ANSI color codes characters
+        String ANSI_green = "\u001B[32m";
+//There are other ANSI codes and formatting characters
+//You can change background colors, add bold text... consult Google.
+//http://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+        String ANSI_black = "\u001B[30m";
+        String ANSI_reset_color = "\u001B[0m";
+
+        char spade =9824;  //There are Unicode characters for playing card symbols
+        char club = 9827;
+        char heart =9829;
+        char diamond = 9830;
+
+        System.out.println(ANSI_green + "this text is green!" + ANSI_reset_color);
+        System.out.println(ANSI_red + "and this is red" + ANSI_reset_color);
+        System.out.println(ANSI_black + "and this is black" + ANSI_reset_color);
+        System.out.println("And back to the default color");
+
+//Put it all together
+        System.out.println(ANSI_black + "The 3 of spades: 3" + spade + ANSI_reset_color);
+        System.out.println(ANSI_red + "The 3 of diamonds: 3" + diamond + ANSI_reset_color);
+    }
+
+    /**
+     * Created by Patrick on 3/23/2016.
+     */
+    public static class Deck {
+
+    }
+}
---
+++ DeckOfCards.java
@@ -0,0 +1,8 @@
+package com.patrick;
+
+/**
+ * Created by Patrick on 3/23/2016.
+ */
+public class DeckOfCards {
+
+}
