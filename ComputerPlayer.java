ca31a5df7dc96fa4acb5c233d8cb6d1e411b626c
ox6185ys <ox6185ys@go.minneapolis.edu>
2016-09-13 09:49:09 -0500

    Crazy Eights Project 1Fall2016

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,11 +1,6 @@
 package com.patrick;
-
-/**
- * Created by Patrick on 3/27/2016.
- */
 public class ComputerPlayer extends Player {
     public Card throwCard(Card dealtCard) {
-
         return null;
-    }
+    }//
 }
